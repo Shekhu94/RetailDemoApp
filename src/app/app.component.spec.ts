0b071f82f10cf558157f4e8bfb2748bfd5f54059
Shekhu94 <sainishekhar314@gmail.com>
2024-08-04 16:13:53 +0530

    ut fix

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,60 @@
 import { TestBed } from '@angular/core/testing';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideRouter } from '@angular/router';
+import {
+  MSAL_GUARD_CONFIG,
+  MSAL_INSTANCE,
+  MsalBroadcastService,
+  MsalGuard,
+  MsalService,
+} from '@azure/msal-angular';
 import { AppComponent } from './app.component';
+import { MSALInstanceFactory, MSALGuardConfigFactory } from './app.config';
+import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
+import { NgxsModule } from '@ngxs/store';
+import { ProfileState } from './store/profile/profile.state';
+import { ProductListState } from './store/products/products.state';
+import { CartState } from './store/cart/cart.state';
+import { environment } from '../environments/environment';
+import { NgxsStoragePluginModule, StorageOption } from '@ngxs/storage-plugin';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        provideRouter(routes),
+        importProvidersFrom(
+          NgxsModule.forRoot([ProfileState, ProductListState, CartState], {
+            developmentMode: !environment.production,
+          }),
+          NgxsStoragePluginModule.forRoot({
+            keys: ['cart'],
+            storage: StorageOption.SessionStorage,
+          })
+        ),
+        importProvidersFrom(
+          NgxsReduxDevtoolsPluginModule.forRoot({
+            disabled: environment.production,
+          })
+        ),
+        MsalService,
+        MsalGuard,
+        MsalBroadcastService,
+        {
+          provide: MSAL_INSTANCE,
+          useFactory: MSALInstanceFactory,
+        },
+        {
+          provide: MSAL_GUARD_CONFIG,
+          useFactory: MSALGuardConfigFactory,
+        },
+      ],
     }).compileComponents();
   });
 
@@ -18,12 +68,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('yCompany');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, yCompany');
   });
 });
--- src/app/pages/checkout/checkout.component.spec.ts
+++ src/app/pages/checkout/checkout.component.spec.ts
@@ -1,6 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CheckoutComponent } from './checkout.component';
+import { RouterModule } from '@angular/router';
+import { StaticdataService } from '../../shared/services/staticdata.service';
+import { NgxsModule, Store } from '@ngxs/store';
+import { importProvidersFrom } from '@angular/core';
+import { ProfileState } from '../../store/profile/profile.state';
+import { ProductListState } from '../../store/products/products.state';
+import { CartState } from '../../store/cart/cart.state';
+import { environment } from '../../../environments/environment';
+import { NgxsStoragePluginModule, StorageOption } from '@ngxs/storage-plugin';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('CheckoutComponent', () => {
   let component: CheckoutComponent;
@@ -8,9 +21,31 @@ describe('CheckoutComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CheckoutComponent]
-    })
-    .compileComponents();
+      imports: [
+        BrowserAnimationsModule,
+        RouterModule.forRoot([]),
+        CheckoutComponent,
+      ],
+      providers: [
+        StaticdataService,
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        importProvidersFrom(
+          NgxsModule.forRoot([ProfileState, ProductListState, CartState], {
+            developmentMode: !environment.production,
+          }),
+          NgxsStoragePluginModule.forRoot({
+            keys: ['cart'],
+            storage: StorageOption.SessionStorage,
+          })
+        ),
+        importProvidersFrom(
+          NgxsReduxDevtoolsPluginModule.forRoot({
+            disabled: environment.production,
+          })
+        ),
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(CheckoutComponent);
     component = fixture.componentInstance;
